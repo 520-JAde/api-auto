64d9f0df3a1d95ec54bd7ab72632462503fa9479
‘youngT’ <228289351@qq.com>
2020-01-09 16:17:31 +0800

    增加接收邮件的收件人数

--- common/sendEmail.py
+++ common/sendEmail.py
@@ -25,7 +25,7 @@ class SendEmail:
         smtpserver = 'smtp.163.com'
         sender = "yt18782452291@163.com"
         password = "YTZY01201216"
-        addresses = [sender,'228289351@qq.com']
+        addresses = [sender,'yangtao@neurongenius.com','fujia@neurongenius.com','liuyuehong@neurongenius.com','zhouxinwei@neurongenius.com']
         # ,'fujia@neurongenius.com'
 
         # 2.编辑邮件
